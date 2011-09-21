62743aec82b7a1a9393a77beb4ac8ebe03734aba
John Heaton <jheaton.dev@gmail.com>
2011-09-20 22:54:45 -0400

    firmware downloader added

---
+++ iosfw.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import urllib2
+import plistlib
+
+print "Downloading info from Apple..."
+rawplist = urllib2.urlopen("http://itunes.com/versions?touchUpdate=true")
+
+plistobj = plistlib.readPlist(rawplist)
+used_urls = []
+
+for appleisdumb in plistobj["MobileDeviceSoftwareVersionsByVersion"]:
+    for sv in plistobj["MobileDeviceSoftwareVersionsByVersion"][appleisdumb]["MobileDeviceSoftwareVersions"]:
+        for build in plistobj["MobileDeviceSoftwareVersionsByVersion"][appleisdumb]["MobileDeviceSoftwareVersions"][sv]:
+            chunk = plistobj["MobileDeviceSoftwareVersionsByVersion"][appleisdumb]["MobileDeviceSoftwareVersions"][sv][build]
+            if(chunk.has_key("Restore")):
+                if(chunk["Restore"]["FirmwareURL"] in used_urls):
+                   continue;
+                print "%s - %s(%s): %s" %(sv, chunk["Restore"]["ProductVersion"], chunk["Restore"]["BuildVersion"], chunk["Restore"]["FirmwareURL"])
+                used_urls.append(chunk["Restore"]["FirmwareURL"])
